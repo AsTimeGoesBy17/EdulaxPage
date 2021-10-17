658952a1172d0e4527aa547e11073acf5a70033f
AsTimeGoesBy <izabella.rosikon@wp.pl>
2021-10-18 00:54:12 +0200

    themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,4 +15,54 @@ module.exports = {
       require('daisyui'),
       require('@tailwindcss/typography')
   ],
+
+  daisyui: {
+    themes: [
+      {
+        'light': {
+          "primary": '#111111',
+          "primary-content": '#223232',
+          "primary-focus": '#432332',
+
+          "secondary": '#111111',
+          "secondary-content": '#223232',
+          "secondary-focus": '#432332',
+
+          "neutral": '#ffffff',
+          "neutral-content": "#123123",
+
+          "base-100": "#ffffff",
+          "base-200": "#eeeeee",
+          "base-300": "#dddddd",
+
+          "info": "#123123",
+          "success": "#00ff00",
+          "warning": "#ff00ff",
+          "error": "#ff0000"
+        },
+        "dark": {
+          "primary": '#111111',
+          "primary-content": '#223232',
+          "primary-focus": '#432332',
+
+          "secondary": '#111111',
+          "secondary-content": '#223232',
+          "secondary-focus": '#432332',
+
+          "neutral": '#ffffff',
+          "neutral-content": "#123123",
+
+          "base-100": "#ffffff",
+          "base-200": "#eeeeee",
+          "base-300": "#dddddd",
+
+          "info": "#123123",
+          "success": "#00ff00",
+          "warning": "#ff00ff",
+          "error": "#ff0000"
+        }
+        
+      }
+    ]
+  }
 }
